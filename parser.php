b744e4ef57c72762ad4d1321c6599b34c8fc25ad
MeTonaTOR <metonator@gmail.com>
2024-01-13 02:20:39 +0100

    Spacebar is also a valid character for chat

--- parser.php
+++ parser.php
@@ -9,7 +9,7 @@ $validchars = array(
     "41", "42", "43", "44", "45", "46", "47", "48", "49", "4a", "4b", "4c", "4d", "4e", "4f", "50", 
     "51", "52", "53", "54", "55", "56", "57", "58", "59", "5a", "5b", "5c", "5d", "5e", "5f", "60", 
     "61", "62", "63", "64", "65", "66", "67", "68", "69", "6a", "6b", "6c", "6d", "6e", "6f", "70", 
-    "71", "72", "73", "74", "75", "76", "77", "78", "79", "7a", "7b", "7c", "7d");
+    "71", "72", "73", "74", "75", "76", "77", "78", "79", "7a", "7b", "7c", "7d", "20");
 
 $teamvalidator = array(
     "10" => "Red",
@@ -23,14 +23,19 @@ $teamvalidator = array(
 $binary = bin2hex(file_get_contents($argv[1]));
 $binary = trim(chunk_split($binary, 2, " "));
 
-$chatinfo = '/0F (00|01|02|03|04|05|06) (00|01|02|03|04|05|06) (f.|0.|1.) (.*?) 00/mi';
+$chatinfo = '/0F (00|01|02|03|04|05|06) (00|01|02|03|04|05|06|FF) (f.|0.|1.) (.*?) 00/mi';
 preg_match_all($chatinfo, $binary, $chat, PREG_SET_ORDER, 0);
 
 foreach ($chat as $text) {
-    if($text[1] == $text[2] && in_array(substr($text[4], 0, 2), $validchars)) {
+    //var_dump($text);
+    if(($text[1] == $text[2] || $text[2] == "ff") && in_array(substr($text[4], 0, 2), $validchars)) {
         switch($text[3]) {
             case 'ff': 
-                echo "[Player ".$text['1']."] ".hex2bin(str_replace(" ", "", $text[4]))."\n"; break;
+                if($text[2] == "ff") {
+                    echo "[Anon by ".$text['1']."] ".hex2bin(str_replace(" ", "", $text[4]))."\n"; break;
+                } else {
+                    echo "[Player ".$text['1']."] ".hex2bin(str_replace(" ", "", $text[4]))."\n"; break;
+                }
             case 'fe': 
                 echo "Player ".$text['1']." is ".hex2bin(str_replace(" ", "", $text[4]))."\n"; break;
             case '10': case '11': case '12': case '13': case '14': case '15':
